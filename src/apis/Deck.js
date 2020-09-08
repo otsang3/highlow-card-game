5e3cf82d929b517f73b8f45427d3bb4b84eacebc
otsang3 <ohhtsang@gmail.com>
2020-09-08 16:54:12 +0100

    coded Deck.js API

--- src/apis/Deck.js
+++ src/apis/Deck.js
@@ -1,13 +1,20 @@
-const deck = [];
+const getDeck = () => {
+    const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    const suits = ["diamonds", "clubs", "hearts", "spades"];
+    const deck = [];
 
-const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-const suits = ["diamonds", "clubs", "hearts", "spades"];
-
-for (suit of suits) {
-    for (value of values) {
-        card = {value: value, suit: suit}
-        deck.push(card)
-    }
+    for(let i = 0; i < suits.length; i++)
+        {
+        for(let x = 0; x < values.length; x++)
+            {
+                let card = {Value: values[x], Suit: suits[i]};
+                deck.push(card);
+            }
+        }
+    return deck;
 }
 
-export const deck;
+export const deck = getDeck();
+
+
+
--- src/contexts/DeckContext.js
+++ src/contexts/DeckContext.js
@@ -1,8 +1,13 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import {deck} from '../apis/Deck';
 
 export const DeckContext = createContext();
 
+console.log(deck);
+
 function DeckContextProvider(props) {
+
+    const [state, setState] = useState(deck)
 
     return(
         <DeckContext.Provider>
