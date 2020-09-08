c61cb9608a786f68689800eb1415c0bc1934a33c
otsang3 <ohhtsang@gmail.com>
2020-09-09 00:40:22 +0100

    added addPoint, gameOver, playGame functions in DeckContext

--- src/contexts/DeckContext.js
+++ src/contexts/DeckContext.js
@@ -16,13 +16,47 @@ function DeckContextProvider(props) {
 
     const [state, setState] = useState(initialState)
 
+    const addPoint = () => {
+        setState(prevState => {
+            return {
+                ...prevState,
+                userScore: prevState.userScore += 1
+            }
+        })
+    }
+
+    const discardCard = () => {
+        const discard = deck.slice(0,1)
+        state.discardPile.unshift(discard)
+    }
+
+    const gameOver = () => {
+        setState(prevState => {
+            return {
+                ...prevState,
+                gameOver: true
+            }
+        })
+    }
+
     const playGame = (string) => {
-        
+        if (state.currentCard.value === state.nextCard.value) {
+            discardCard();
+        } else if (state.currentCard.value > state.nextCard.value && string === "HIGHER") {
+            discardCard();
+        } else if (state.currentCard.value < state.nextCard.value && string === "LOWER") {
+            discardCard();
+        } else {
+            gameOver();
+        }
     }
 
     return(
         <DeckContext.Provider value={{
-            state: state
+            addPoint: addPoint,
+            gameOver: gameOver,
+            playGame: playGame,
+            state: state  
         }}>
             {props.children}
         </DeckContext.Provider>
